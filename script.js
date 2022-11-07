cabe5a0ad2935e6845d622e56367a872b28cd231
wyamet <wyattdomanski.md@gmail.com>
2022-11-06 17:29:16 -0800

    worked on linking form from html to java script, had some errors, returning to main branch to complete with out exccess work.

--- script.js
+++ script.js
@@ -1,13 +1,63 @@
-const characterRange = document.getElementById
-('characterRange')
-const characterAmountNumber = document.getElementById
+const characterRange = document.getElementById('characterRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById('paswordGeneratorForm')
-('characterAmountNumber')
+const passwordDisplay = document.getElementById('paswordDisplay')
+
+//refered to ASCII character sheet//
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47
+).concat(arrayFromLowToHigh (58,64)
+).concat(arrayFromLowToHigh (91, 96)
+).concat(arrayFromLowToHigh (123, 126)
+)
+
 
 // Creating event listeners for the form inputs//
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+    const password = generatePassword(characterAmountNumber, includeUppercase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+    
+} )
+
+function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase ) {
+    let charCodes = LOWERCASE_CHAR_CODES
+    if ( includeUppercase) charCodes = charCodes.concat (UPPERCASE_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat (NUMBER_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat (SYMBOL_CHAR_CODES)
+    
+    const passwordCharacters = []
+    for (let i = 0; i < characterAmount; i++) {
+        const characterCode =charCodes[Math.floor(Math.random() * characterAmount)]
+        passwordCharacters.push(string.fromCharCode(characterCode))
+
+    }
+    return passwordCharacters.join('')
+    
+}
+
+function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = low; i < high; i++) {
+        array.push(i)
+    }
+    return array
+}
+
 
 function syncCharacterAmount (e) {
     const value = e.target.value
